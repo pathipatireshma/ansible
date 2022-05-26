3d5f0d499c3e6d13052e50919d1d87e33cb15b1e
pathipatireshma <pathipati.reshma@gmail.com>
2022-05-26 12:18:22 +0530

    ec2

--- create-ec2.sh
+++ create-ec2.sh
@@ -56,8 +56,8 @@ INSTANCE_CREATE() {
 
 ### Main Program
 
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
-#AMI_ID=ami-0a962f8fe6f3436d1
+#AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
+AMI_ID=ami-0fa36b8dda55433bc
 
 if [ "$1" == "list" ]; then
   aws ec2 describe-instances  --query "Reservations[*].Instances[*].{PrivateIP:PrivateIpAddress,PublicIP:PublicIpAddress,Name:Tags[?Key=='Name']|[0].Value,Status:State.Name}"  --output table
